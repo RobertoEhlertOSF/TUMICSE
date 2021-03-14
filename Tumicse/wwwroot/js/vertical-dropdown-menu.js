9a71b5e3a8803a732c5ccc3a1cf1eb7f7b3478fb
oldbetu <63211875+oldbetu@users.noreply.github.com>
2021-03-14 20:26:16 -0300

    Setup init

---
+++ Tumicse/wwwroot/js/vertical-dropdown-menu.js
@@ -0,0 +1,8 @@
+﻿
+$(document).ready(function () {
+	$('.sub-menu ul').hide();
+	$(".sub-menu a").click(function () {
+		$(this).parent(".sub-menu").children("ul").slideToggle("100");
+		$(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+	});
+});
