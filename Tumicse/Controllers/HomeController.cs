b65c2c93991b571c4da5f6b6079286b703b2c27f
oldbetu <63211875+oldbetu@users.noreply.github.com>
2021-03-26 01:22:02 -0300

    finished

--- Tumicse/Controllers/HomeController.cs
+++ Tumicse/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Logging;
 using Tumicse.Models;
 
@@ -20,8 +21,13 @@ namespace Tumicse.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string urlVideo)
         {
+            if (urlVideo == null)
+            {
+                urlVideo = "https://www.youtube-nocookie.com/embed/sRPx6VTLMmw";
+            }
+            ViewBag.urlVideo = urlVideo + "?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1";
             return View();
         }
 
